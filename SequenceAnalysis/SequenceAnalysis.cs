577707d4ca9040ebc76f9e44641ac5b8a7a5edd7
onurhenden <onur.henden@metu.edu.tr>
2022-03-18 19:30:39 +0300

    add sequence analysis

---
+++ SequenceAnalysis/SequenceAnalysis.cs
@@ -0,0 +1,19 @@
+﻿namespace SequenceAnalysisLibrary
+{
+    public class SequenceAnalysis
+    {
+        public static string Analyze(string input)
+        {
+            if (input.Length == 0)
+                return string.Empty;
+
+            var uppercaseLetters = input.Where(x => char.IsUpper(x)).ToArray<char>();
+            if (uppercaseLetters.Length == 0)
+                return string.Empty;
+
+            Array.Sort(uppercaseLetters);
+            return new string(uppercaseLetters);
+
+        }
+    }
+}
